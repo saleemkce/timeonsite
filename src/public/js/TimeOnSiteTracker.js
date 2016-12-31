4bebdfb50a3338b7ba81d79f8604d5e530529c96
Saleem Khan <saleemkce@gmail.com>
2016-12-31 14:46:29 +0530

    License and terms of use added in repository and included in file source

--- src/public/js/TimeOnSiteTracker.js
+++ src/public/js/TimeOnSiteTracker.js
@@ -1,27 +1,18 @@
 /**
- * @preserve
- * The MIT License (MIT)
+ * Commercial License
  *
  * TimeOnSiteTracker.js - Measure your user's Time on site accurately.
  * 
  * Copyright (C) 2016  Saleem Khan
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
- * software and associated documentation files (the "Software"), to deal in the Software 
- * without restriction, including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
- * to permit persons to whom the Software is furnished to do so, subject to the following 
- * conditions:
-
- * The above copyright notice and this permission notice shall be included in all copies 
- * or substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * License key: {your-license-key-here}
+ *
+ * This is copyrighted software and requires you buy commericial license
+ * before using it. Replace {your-license-key-here} with the licence key 
+ * that you buy before starting to use this software. 
+ * 
+ * visit https://github.com/saleemkce/timeonsite/master/LICENSE.md to learn more about 
+ * license terms. 
  */
 
 /**
