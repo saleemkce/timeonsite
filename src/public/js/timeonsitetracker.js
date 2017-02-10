9ab6eba88d89f3fb75212819325d740cfb345d03
Saleem Khan <saleemkce@gmail.com>
2017-02-10 22:33:06 +0530

    Fixed cookie name clash in multiple sections in a single domain

--- src/public/js/timeonsitetracker.js
+++ src/public/js/timeonsitetracker.js
@@ -81,6 +81,13 @@ var TimeOnSiteTracker = function(config) {
     this.isRequestHeadersAvailable = false;
 
     this.developerMode = false;
+
+    this.TOS_CONST = {
+        TOSSessionKey: 'TOSSessionKey',
+        TOSSessionDuration: 'TOSSessionDuration',
+        TOSUserId: 'TOSUserId',
+        TOSAnonSessionRefresh: 'TOSAnonSessionRefresh'
+    }
     
     console.log('Time at page entry: ' + this.varyingStartTime);
 
@@ -135,6 +142,19 @@ TimeOnSiteTracker.prototype.initialize = function(config) {
         this.TOSCookie.customCookieString += 'path=/;';
     }
 
+    /**
+     * if either domain name or path is set by user for TOS cookie, then TOS cookie 
+     * names are suffixed unique string to avoid cookie name clashes that may 
+     * raise when another page sets base path "/" in TOS cookie.
+     */
+    if(this.TOSCookie.customCookieString && this.TOSCookie.customCookieString != 'path=/;') {
+        var TOSCookieSuffix = this.MD5(this.TOSCookie.customCookieString);
+        this.TOS_CONST.TOSSessionKey = this.TOS_CONST.TOSSessionKey + '_' + TOSCookieSuffix;
+        this.TOS_CONST.TOSSessionDuration = this.TOS_CONST.TOSSessionDuration + '_' + TOSCookieSuffix;
+        this.TOS_CONST.TOSUserId = this.TOS_CONST.TOSUserId + '_' + TOSCookieSuffix;
+        this.TOS_CONST.TOSAnonSessionRefresh = this.TOS_CONST.TOSAnonSessionRefresh + '_' + TOSCookieSuffix;
+    }
+
     if(config && config.request && config.request.url) {
         this.request.url = config.request.url;
         this.isURLValid(this.request.url);
@@ -171,7 +191,7 @@ TimeOnSiteTracker.prototype.initialize = function(config) {
     //Enable "developer mode" to view TOS real-time internal data and logs
     if (config && config.developerMode) {
         this.developerMode = true;
-        console.info('TOS cookie created with path/domain :' + this.TOSCookie.customCookieString);
+        console.info('TOS cookie created with path/domain : ' + this.TOSCookie.customCookieString);
     }
 
     // create and monitor TOS session
@@ -276,9 +296,9 @@ TimeOnSiteTracker.prototype.startSession = function(userId) {
          *  TOSAnonSessionRefresh = 0 (Session is authenticated. Anonymous session cookie 
          *  is not allowed to be refreshed periodically)
          */
-        this.setCookie('TOSAnonSessionRefresh', 0, this.sessionValidity.oneDayInSecs);
+        this.setCookie(this.TOS_CONST.TOSAnonSessionRefresh, 0, this.sessionValidity.oneDayInSecs);
         this.TOSUserId = userId;
-        this.setCookie('TOSUserId', userId, this.sessionValidity.oneDayInSecs);
+        this.setCookie(this.TOS_CONST.TOSUserId, userId, this.sessionValidity.oneDayInSecs);
         this.createNewSession();
     } else {
         console.warn('Please give proper userId to start TOS session.');
@@ -292,9 +312,9 @@ TimeOnSiteTracker.prototype.endSession = function() {
     this.processTOSData();
 
     // remove session data
-    this.removeCookie('TOSUserId');
-    this.removeCookie('TOSSessionKey');
-    this.removeCookie('TOSSessionDuration');
+    this.removeCookie(this.TOS_CONST.TOSUserId);
+    this.removeCookie(this.TOS_CONST.TOSSessionKey);
+    this.removeCookie(this.TOS_CONST.TOSSessionDuration);
 
     //create new TOS session
     this.TOSUserId = 'anonymous';
@@ -325,8 +345,8 @@ TimeOnSiteTracker.prototype.initBlacklistUrlConfig = function(config) {
 };
 
 TimeOnSiteTracker.prototype.monitorUser = function() {
-    var authenticatedUser = this.getCookie('TOSUserId'),
-        sessionKey = this.getCookie('TOSSessionKey');
+    var authenticatedUser = this.getCookie(this.TOS_CONST.TOSUserId),
+        sessionKey = this.getCookie(this.TOS_CONST.TOSSessionKey);
 
     if(authenticatedUser && authenticatedUser.length) {
         console.info('Authenticated user!!!');
@@ -341,8 +361,8 @@ TimeOnSiteTracker.prototype.monitorUser = function() {
 };
 
 TimeOnSiteTracker.prototype.monitorSession = function() {
-    var sessionDuration = this.getCookie('TOSSessionDuration'),
-        sessionKey = this.getCookie('TOSSessionKey'),
+    var sessionDuration = this.getCookie(this.TOS_CONST.TOSSessionDuration),
+        sessionKey = this.getCookie(this.TOS_CONST.TOSSessionKey),
         pageData,
         count = 0;
 
@@ -356,21 +376,21 @@ TimeOnSiteTracker.prototype.monitorSession = function() {
     //console.error('count : ' + ' top : ' + pageData.timeOnPage + 'sessDura: ' + sessionDuration);
     count = pageData.timeOnPage + sessionDuration;
     this.TOSSessionKey = sessionKey;
-    this.setCookie('TOSSessionDuration', count, this.sessionValidity.oneDayInSecs);
+    this.setCookie(this.TOS_CONST.TOSSessionDuration, count, this.sessionValidity.oneDayInSecs);
 
     this.timeOnSite = count;
 
 };
 
 TimeOnSiteTracker.prototype.createNewSession = function(userType) {
-    this.setCookie('TOSSessionDuration', 0, this.sessionValidity.oneDayInSecs);
+    this.setCookie(this.TOS_CONST.TOSSessionDuration, 0, this.sessionValidity.oneDayInSecs);
     this.TOSSessionKey = this.createTOSSessionKey();
     //alert('new cookie created!!!');
     
     if(userType === 'anonymous') {
         //alert('user type is anonymous');
-        this.setCookie('TOSSessionKey', this.TOSSessionKey, this.sessionValidity.anonymous);
-        this.setCookie('TOSAnonSessionRefresh', 1, this.sessionValidity.oneDayInSecs);
+        this.setCookie(this.TOS_CONST.TOSSessionKey, this.TOSSessionKey, this.sessionValidity.anonymous);
+        this.setCookie(this.TOS_CONST.TOSAnonSessionRefresh, 1, this.sessionValidity.oneDayInSecs);
         this.renewSession();
     } else {//alert('user type is authenticated!');
         if(this.anonymousTimerId) {
@@ -378,7 +398,7 @@ TimeOnSiteTracker.prototype.createNewSession = function(userType) {
             console.info('Timer cleared '+this.anonymousTimerId);
         } else {alert('Timer not found '+this.anonymousTimerId);}
         //authenticated users session extends till the next day
-        this.setCookie('TOSSessionKey', this.TOSSessionKey, this.sessionValidity.oneDayInSecs);
+        this.setCookie(this.TOS_CONST.TOSSessionKey, this.TOSSessionKey, this.sessionValidity.oneDayInSecs);
     }
 
     this.timeOnSite = 0;
@@ -390,13 +410,13 @@ TimeOnSiteTracker.prototype.renewSession = function() {
         refreshState;
     this.anonymousTimerId = setInterval(function() {
 
-        refreshState = self.getCookie('TOSAnonSessionRefresh');
+        refreshState = self.getCookie(self.TOS_CONST.TOSAnonSessionRefresh);
         /**
          * Checks if user session is anonymous before altering cookie 
          * lifetime for preventing multi-tab session overwrite issue 
          */
         if (refreshState == 1) {
-            self.setCookie('TOSSessionKey', self.TOSSessionKey, self.sessionValidity.anonymous);
+            self.setCookie(self.TOS_CONST.TOSSessionKey, self.TOSSessionKey, self.sessionValidity.anonymous);
 
             if (self.developerMode) {
                 console.log('Cookie renewed at : '+(new Date()));
@@ -483,6 +503,8 @@ TimeOnSiteTracker.prototype.setCustomData = function(data) {
 TimeOnSiteTracker.prototype.unsetCustomData = function() {
     this.customData = null;
 };
+
+TimeOnSiteTracker.prototype.MD5 = function(s){function L(k,d){return(k<<d)|(k>>>(32-d))}function K(G,k){var I,d,F,H,x;F=(G&2147483648);H=(k&2147483648);I=(G&1073741824);d=(k&1073741824);x=(G&1073741823)+(k&1073741823);if(I&d){return(x^2147483648^F^H)}if(I|d){if(x&1073741824){return(x^3221225472^F^H)}else{return(x^1073741824^F^H)}}else{return(x^F^H)}}function r(d,F,k){return(d&F)|((~d)&k)}function q(d,F,k){return(d&k)|(F&(~k))}function p(d,F,k){return(d^F^k)}function n(d,F,k){return(F^(d|(~k)))}function u(G,F,aa,Z,k,H,I){G=K(G,K(K(r(F,aa,Z),k),I));return K(L(G,H),F)}function f(G,F,aa,Z,k,H,I){G=K(G,K(K(q(F,aa,Z),k),I));return K(L(G,H),F)}function D(G,F,aa,Z,k,H,I){G=K(G,K(K(p(F,aa,Z),k),I));return K(L(G,H),F)}function t(G,F,aa,Z,k,H,I){G=K(G,K(K(n(F,aa,Z),k),I));return K(L(G,H),F)}function e(G){var Z;var F=G.length;var x=F+8;var k=(x-(x%64))/64;var I=(k+1)*16;var aa=Array(I-1);var d=0;var H=0;while(H<F){Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=(aa[Z]| (G.charCodeAt(H)<<d));H++}Z=(H-(H%4))/4;d=(H%4)*8;aa[Z]=aa[Z]|(128<<d);aa[I-2]=F<<3;aa[I-1]=F>>>29;return aa}function B(x){var k="",F="",G,d;for(d=0;d<=3;d++){G=(x>>>(d*8))&255;F="0"+G.toString(16);k=k+F.substr(F.length-2,2)}return k}function J(k){k=k.replace(/rn/g,"n");var d="";for(var F=0;F<k.length;F++){var x=k.charCodeAt(F);if(x<128){d+=String.fromCharCode(x)}else{if((x>127)&&(x<2048)){d+=String.fromCharCode((x>>6)|192);d+=String.fromCharCode((x&63)|128)}else{d+=String.fromCharCode((x>>12)|224);d+=String.fromCharCode(((x>>6)&63)|128);d+=String.fromCharCode((x&63)|128)}}}return d}var C=Array();var P,h,E,v,g,Y,X,W,V;var S=7,Q=12,N=17,M=22;var A=5,z=9,y=14,w=20;var o=4,m=11,l=16,j=23;var U=6,T=10,R=15,O=21;s=J(s);C=e(s);Y=1732584193;X=4023233417;W=2562383102;V=271733878;for(P=0;P<C.length;P+=16){h=Y;E=X;v=W;g=V;Y=u(Y,X,W,V,C[P+0],S,3614090360);V=u(V,Y,X,W,C[P+1],Q,3905402710);W=u(W,V,Y,X,C[P+2],N,606105819);X=u(X,W,V,Y,C[P+3],M,3250441966);Y=u(Y,X,W,V,C[P+4],S,4118548399);V=u(V,Y,X,W,C[P+5],Q,1200080426);W=u(W,V,Y,X,C[P+6],N,2821735955);X=u(X,W,V,Y,C[P+7],M,4249261313);Y=u(Y,X,W,V,C[P+8],S,1770035416);V=u(V,Y,X,W,C[P+9],Q,2336552879);W=u(W,V,Y,X,C[P+10],N,4294925233);X=u(X,W,V,Y,C[P+11],M,2304563134);Y=u(Y,X,W,V,C[P+12],S,1804603682);V=u(V,Y,X,W,C[P+13],Q,4254626195);W=u(W,V,Y,X,C[P+14],N,2792965006);X=u(X,W,V,Y,C[P+15],M,1236535329);Y=f(Y,X,W,V,C[P+1],A,4129170786);V=f(V,Y,X,W,C[P+6],z,3225465664);W=f(W,V,Y,X,C[P+11],y,643717713);X=f(X,W,V,Y,C[P+0],w,3921069994);Y=f(Y,X,W,V,C[P+5],A,3593408605);V=f(V,Y,X,W,C[P+10],z,38016083);W=f(W,V,Y,X,C[P+15],y,3634488961);X=f(X,W,V,Y,C[P+4],w,3889429448);Y=f(Y,X,W,V,C[P+9],A,568446438);V=f(V,Y,X,W,C[P+14],z,3275163606);W=f(W,V,Y,X,C[P+3],y,4107603335);X=f(X,W,V,Y,C[P+8],w,1163531501);Y=f(Y,X,W,V,C[P+13],A,2850285829);V=f(V,Y,X,W,C[P+2],z,4243563512);W=f(W,V,Y,X,C[P+7],y,1735328473);X=f(X,W,V,Y,C[P+12],w,2368359562);Y=D(Y,X,W,V,C[P+5],o,4294588738);V=D(V,Y,X,W,C[P+8],m,2272392833);W=D(W,V,Y,X,C[P+11],l,1839030562);X=D(X,W,V,Y,C[P+14],j,4259657740);Y=D(Y,X,W,V,C[P+1],o,2763975236);V=D(V,Y,X,W,C[P+4],m,1272893353);W=D(W,V,Y,X,C[P+7],l,4139469664);X=D(X,W,V,Y,C[P+10],j,3200236656);Y=D(Y,X,W,V,C[P+13],o,681279174);V=D(V,Y,X,W,C[P+0],m,3936430074);W=D(W,V,Y,X,C[P+3],l,3572445317);X=D(X,W,V,Y,C[P+6],j,76029189);Y=D(Y,X,W,V,C[P+9],o,3654602809);V=D(V,Y,X,W,C[P+12],m,3873151461);W=D(W,V,Y,X,C[P+15],l,530742520);X=D(X,W,V,Y,C[P+2],j,3299628645);Y=t(Y,X,W,V,C[P+0],U,4096336452);V=t(V,Y,X,W,C[P+7],T,1126891415);W=t(W,V,Y,X,C[P+14],R,2878612391);X=t(X,W,V,Y,C[P+5],O,4237533241);Y=t(Y,X,W,V,C[P+12],U,1700485571);V=t(V,Y,X,W,C[P+3],T,2399980690);W=t(W,V,Y,X,C[P+10],R,4293915773);X=t(X,W,V,Y,C[P+1],O,2240044497);Y=t(Y,X,W,V,C[P+8],U,1873313359);V=t(V,Y,X,W,C[P+15],T,4264355552);W=t(W,V,Y,X,C[P+6],R,2734768916);X=t(X,W,V,Y,C[P+13],O,1309151649);Y=t(Y,X,W,V,C[P+4],U,4149444226);V=t(V,Y,X,W,C[P+11],T,3174756917);W=t(W,V,Y,X,C[P+2],R,718787259);X=t(X,W,V,Y,C[P+9],O,3951481745);Y=K(Y,h);X=K(X,E);W=K(W,v);V=K(V,g)}var i=B(Y)+B(X)+B(W)+B(V);return i.toLowerCase()};
 
 /**
  * [resetActivity It is used for both initializing and resetting activity varibales]
@@ -870,7 +892,7 @@ TimeOnSiteTracker.prototype.setCookie = function(cname, cvalue, secs) {
     //console.log('cookie expire @ '+new Date(d));
     
     expires = 'expires=' + d.toUTCString();
-    cookieString = cname + '=' + cvalue + ';' + expires + ';' + this.TOSCookie.customCookieString;
+    cookieString = cname + '=' + cvalue + '; ' + expires + '; ' + this.TOSCookie.customCookieString;
 
     //document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
     document.cookie = cookieString;
@@ -894,7 +916,7 @@ TimeOnSiteTracker.prototype.getCookie = function(cname) {
 
 TimeOnSiteTracker.prototype.removeCookie = function(cname) {
     if(this.getCookie(cname)) {
-        document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
+        document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; ' + this.TOSCookie.customCookieString;
     }
 };
 
